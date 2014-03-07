563c6cbcc569b2b3a643ed32619c57dcb723674a
ka <ka.kaosf@gmail.com>
2014-03-08 04:59:49 +0900

    Don't use "content-type" and "response" functions in "layout.clj"
    They confused me a lot.

--- src/precure_dcd/views/layout.clj
+++ src/precure_dcd/views/layout.clj
@@ -1,7 +1,6 @@
 (ns precure-dcd.views.layout
   (:require [selmer.parser :as parser]
             [clojure.string :as s]
-            [ring.util.response :refer [content-type response]]
             [compojure.response :refer [Renderable]]
             [noir.session :as session]))
 
@@ -13,19 +12,18 @@
   Renderable
   (render
     [this request]
-    (content-type
-      (->>
-        (assoc
-          params
-          (keyword (s/replace template #".html" "-selected"))
-          "active"
-          :servlet-context
-          (:context request)
-          :user-id
-          (session/get :user-id))
-        (parser/render-file (str template-path template))
-        response)
-      "text/html; charset=utf-8")))
+    {:status 200
+     :headers {"Content-Type" "text/html; charset=utf-8"}
+     :body (parser/render-file
+             (str template-path template)
+             (assoc
+               params
+               (keyword (s/replace template #".html" "-selected"))
+               "active"
+               :servlet-context
+               (:context request)
+               :user-id
+               (session/get :user-id)))}))
 
 (defn render [template & [params]]
   (RenderableTemplate. template params))
